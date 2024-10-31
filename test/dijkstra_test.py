7988f1c5fe1f49650f0f1ce5c6c036887b148d3c
7minutech <elijohnson1107@gmail.com>
2024-10-31 12:52:32 -0500

    fix: fixed imports for tests

--- test/dijkstra_test.py
+++ test/dijkstra_test.py
@@ -1,30 +1,33 @@
 #run command python -m pytest
+
 from lib.dijkstra import Dijkstra
 from lib.node import Node
 import math
 
-def add_nodes():
+def test_add_nodes():
     starting_node = Node("A")
     node_1 = Node("B")
     node_2 = Node("C")
     node_3 = Node("D")
     node_4 = Node("E")
-    nodes = [starting_node,node_1,node_2,node_3,node_4]
+    nodes = [starting_node, node_1, node_2, node_3, node_4]
     algo = Dijkstra(starting_node)
     algo.add_nodes(nodes)
     assert algo.nodes[0] == starting_node
     assert algo.nodes[-1] == node_4
-def set_unknown():
+
+def test_set_unknown():
     starting_node = Node("A")
     node_1 = Node("B")
     node_2 = Node("C")
-    nodes = [starting_node,node_1,node_2]
+    nodes = [starting_node, node_1, node_2]
     algo = Dijkstra(starting_node)
     algo.add_nodes(nodes)
-    algo.set_unknown
+    algo.set_unknown()  # Corrected: Added parentheses
     assert algo.links_from_starting_node["B"] == [math.inf, None]
     assert algo.links_from_starting_node["C"] == [math.inf, None]
-def set_links_from_start():
+
+def test_set_links_from_start():
     starting_node = Node("A")
     node_1 = Node("B")
     node_2 = Node("C")
@@ -33,15 +36,16 @@ def set_links_from_start():
     starting_node.add_link("C", 4)
     node_1.add_link("D", 4)
     node_2.add_link("D", 3)
-    nodes = [starting_node,node_1,node_2,node_3]
+    nodes = [starting_node, node_1, node_2, node_3]
     algo = Dijkstra(starting_node)
     algo.add_nodes(nodes)
     algo.set_unknown()
-    algo.set_shortest_path()
+    algo.set_shortest_path()  # Assuming this method is defined correctly
     assert algo.links_from_node["A"] == [0, None]
     assert algo.links_from_node["B"] == [5, "A"]
     assert algo.links_from_node["C"] == [4, "A"]
     assert algo.links_from_node["D"] == [7, "B"]
-    
+
+
 
 
--- test/node_test.py
+++ test/node_test.py
@@ -1,4 +1,5 @@
 #run command python -m pytest
+
 from lib.node import Node
 def test_add_link():
     '''Adds link with value'''
@@ -8,5 +9,5 @@ def test_add_link():
     '''Works with more than 1 link'''
     node.add_link("C",4)
     node.add_link("D",3)
-    assert node.link["C"] == 4
-    assert node.link["D"] == 5
+    assert node.links["C"] == 4
+    assert node.links["D"] == 3
