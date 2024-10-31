5f176b117471f59c1c3d306b9ca8ba0ad081a992
7minutech <elijohnson1107@gmail.com>
2024-10-31 11:37:05 -0500

    feat: added test for add_nodes and set_unknown
    Must be able to hold list of nodes so that we
    can set them to unknown upon init

---
+++ test/dijkstra_test.py
@@ -0,0 +1,26 @@
+#run command python -m pytest
+from lib.dijkstra import Dijkstra
+from lib.node import Node
+
+def add_nodes():
+    starting_node = Node("A")
+    node_1 = Node("B")
+    node_2 = Node("C")
+    node_3 = Node("D")
+    node_4 = Node("E")
+    nodes = [starting_node,node_1,node_2,node_3,node_4]
+    algo = Dijkstra(starting_node)
+    algo.add_nodes(nodes)
+    assert algo.nodes[0] == starting_node
+    assert algo.nodes[-1] == node_4
+def set_unknown():
+    starting_node = Node("A")
+    node_1 = Node("B")
+    node_2 = Node("C")
+    nodes = [starting_node,node_1,node_2]
+    algo = Dijkstra(starting_node)
+    algo.add_nodes(nodes)
+    algo.set_unknown
+    assert algo.links_from_starting_node["B"] == [-1, None]
+    assert algo.links_from_starting_node["C"] == [-1, None]
+
