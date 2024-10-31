7d5ddb5120e26df44a140c44512d31ce4b2755c0
7minutech <elijohnson1107@gmail.com>
2024-10-31 12:19:40 -0500

    feat: added test for setting links from starting
    Must be able to set values for shortest path to
    each node

--- test/dijkstra_test.py
+++ test/dijkstra_test.py
@@ -24,4 +24,24 @@ def set_unknown():
     algo.set_unknown
     assert algo.links_from_starting_node["B"] == [math.inf, None]
     assert algo.links_from_starting_node["C"] == [math.inf, None]
+def set_links_from_start():
+    starting_node = Node("A")
+    node_1 = Node("B")
+    node_2 = Node("C")
+    node_3 = Node("D")
+    starting_node.add_link("B", 5)
+    starting_node.add_link("C", 4)
+    node_1.add_link("D", 4)
+    node_2.add_link("D", 3)
+    nodes = [starting_node,node_1,node_2,node_3]
+    algo = Dijkstra(starting_node)
+    algo.add_nodes(nodes)
+    algo.set_unknown()
+    algo.set_shortest_path()
+    assert algo.links_from_node["A"] == [0, None]
+    assert algo.links_from_node["B"] == [5, "A"]
+    assert algo.links_from_node["C"] == [4, "A"]
+    assert algo.links_from_node["D"] == [7, "B"]
+    
+
 
