6221f257c64bba295ad6df9d4f8a07f4140680bf
7minutech <elijohnson1107@gmail.com>
2024-11-01 22:03:11 -0500

    refactor: changed from label to node object

--- test/node_test.py
+++ test/node_test.py
@@ -5,10 +5,13 @@ from lib.node import Node
 def test_add_link():
     '''Adds link with value'''
     node =  Node("A")
-    node.add_link("B",5)
-    assert node.links["B"] == 5
+    node_2 = Node("B")
+    node.add_link(node_2,5)
+    assert node.links[node_2] == 5
     '''Works with more than 1 link'''
-    node.add_link("C",4)
-    node.add_link("D",3)
-    assert node.links["C"] == 4
-    assert node.links["D"] == 3
+    node_3 = Node("C")
+    node_4 = Node("D")
+    node.add_link(node_3,4)
+    node.add_link(node_4,3)
+    assert node.links[node_3] == 4
+    assert node.links[node_4] == 3
